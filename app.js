90b9d2b0b6964001835e69e02ecbbedaa86bf271
Dmitriy Bezrukov <dima1989o-z@yandex.ru>
2023-10-11 12:58:45 +0300

    Поменял порт для настройки

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const errorHandler = require('./middlewares/errorHandler');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3003, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
